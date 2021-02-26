e8fb727d545af7f74f7c1ca43a60885a342ed997
Ludvig Sellebråten <l.sellebraten@gmail.com>
2021-02-26 12:50:24 +0100

    First commit

---
+++ script.py
@@ -0,0 +1,17 @@
+ """
+ A Program that stores this book information:
+ Title, Author
+ Year, ISBN
+
+ User can:
+ View all records
+ Search an entry
+ Add entry
+ Update entry
+ Delete
+ Close
+ """
+
+from tkinter import *
+
+ window=Tk()
